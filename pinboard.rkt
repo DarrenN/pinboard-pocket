778dcab13d43dfdb75bf38ffc3b9b8e824aea341
DarrenN <info@v25media.com>
2016-11-26 17:21:11 -0500

    updated to use my fork of requests

--- pinboard.rkt
+++ pinboard.rkt
@@ -1,37 +1,31 @@
 #lang racket
 (require json
-         net/http-client
          net/url
-         net/url-structs)
+         request)
 
-(provide get-recent-pinboard
-         pinboard-api-token!)
+(provide get-recent-pinboard)
 
 (define HOST "https://api.pinboard.in/v1/")
 (define PINBOARD-POSTS-PATH "posts/recent")
 
-(define pinboard-api-token "")
+(define pinboard-api-token (make-parameter ""))
 
 (define (pinboard-api-token! token)
   (set! pinboard-api-token token))
 
 (define (make-query pairs)
-  (string-join
-   (map (λ (p) (string-join p "="))
-        (cons (list "format" "json")
-              (cons (list "auth_token" pinboard-api-token) pairs))) "&"))
-
+  ;; Use ENV vars
+  (parameterize ([pinboard-api-token (getenv "PINBOARD_API_TOKEN")])
+    (string-join
+     (map (λ (p) (string-join p "="))
+          (cons (list "format" "json")
+                (cons (list "auth_token" (pinboard-api-token)) pairs))) "&")))
+  
 (define (make-uri path tag)
   (string->url
    (format "~a~a?~a" HOST path (make-query `(("tag" ,tag))))))
 
-(define (get-pinboard method [tag "readlater"])
-  (define-values (status header response)
-    (http-sendrecv/url (make-uri method tag)))
-  (if (equal? "HTTP/1.1 200 OK" (bytes->string/utf-8 status))
-      (read-json response)
-      (raise-user-error
-       'get-pinboard "request to ~a failed: ~a" method status)))
+(define https-json (make-https-requester json-requester))
 
 (define (get-recent-pinboard [tag "readlater"])
-  (get-pinboard PINBOARD-POSTS-PATH tag))
+  (get https-json (make-uri PINBOARD-POSTS-PATH tag)))
---
+++ pocket-api.rkt
@@ -0,0 +1,50 @@
+#lang racket
+(require request
+         net/url
+         json)
+
+(provide add-pocket-urls
+         fetch-pocket-urls)
+
+(define HOST "https://getpocket.com/v3")
+(define PATH_OAUTH_REQUEST "/oauth/request")
+(define PATH_OAUTH_AUTHORIZE "/oauth/request")
+(define PATH_RETRIEVE "/get.php")
+(define PATH_ADD "/add.php")
+(define PATH_MODIFY "/send.php")
+
+(define pocket-access-token (make-parameter ""))
+(define pocket-consumer-key (make-parameter ""))
+
+(define (auth-payload h)
+  ;; Use ENV vars
+  (parameterize ([pocket-access-token (getenv "POCKET_ACCESS_TOKEN")]
+                 [pocket-consumer-key (getenv "POCKET_CONSUMER_KEY")])    
+    (hash-set* h
+               'access_token (pocket-access-token)
+               'consumer_key (pocket-consumer-key))))
+
+(define (make-url path)
+  (string->url (format "~a~a" HOST path)))
+
+(define https-json (make-https-requester json-requester))
+
+;; Return a hash-table of urls that have been archived, sorted
+;; newest first. Use to remove Pinboard URLs from payload.
+(define (fetch-pocket-urls)
+  (define archived
+    (post https-json
+          (make-url PATH_RETRIEVE)
+          (auth-payload
+           (hasheq 'detailType "simple"
+                   'state "all"))))
+  
+  ;; build a hash-table of archived URLs
+  (for/hash ([url (hash-values (hash-ref archived 'list))])
+    (values (hash-ref url 'given_url) 1)))
+
+;; Post a JSON list of URLs to add to Pocket
+(define (add-pocket-urls jsexpr)
+  (post https-json
+        (make-url PATH_MODIFY)
+        (auth-payload jsexpr)))
